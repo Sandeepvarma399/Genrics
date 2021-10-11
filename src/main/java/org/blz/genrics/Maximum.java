cda0d3a6fa23d0eb28ea42014c5adf39437876f3
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-11 20:52:52 +0530

    Added: rafactoring code for genrics

--- src/main/java/org/blz/genrics/Maximum.java
+++ src/main/java/org/blz/genrics/Maximum.java
@@ -4,40 +4,16 @@ package org.blz.genrics;
 public class Maximum {
     public static void main(String[] args) {
         Integer a = 30, b = 10, c = 20;
-      Float d = 40f,e = 10f,f= 30f;
-       String s1 = "apple",s2 = "peach", s3 = "banana";
+        Float d = 40f, e = 10f, f = 30f;
+        String s1 = "apple", s2 = "peach", s3 = "banana";
         getMaximum(a, b, c);
-        getMaximum(a, b, c);
-        getMaximum( a,b,c);
+        getMaximum(d, e, f);
+        getMaximum(s1, s2, s3);
 
     }
 
-    public static Integer getMaximum(Integer a, Integer b, Integer c) {
-        Integer max = b;
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-           max = a;
-       } else if (c.compareTo(max) > 0) {
-            max = c;
-        }
-
-       System.out.println(max);
-       return max;
-    }
-
-
-   public static Float getMaximum(Float a, Float b, Float c) {
-        Float max = b;
-       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-            max = a;
-        } else if (c.compareTo(max) > 0) {
-            max = c;
-        }
-       System.out.println(max);
-        return max;
-  }
-
-    public static String getMaximum( String a, String b, String c) {
-        String max = b;
+    public static  <E extends Comparable> E getMaximum(E a, E b, E c) {
+        E max = b;
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
             max = a;
         } else if (c.compareTo(max) > 0) {
@@ -46,10 +22,36 @@ public class Maximum {
 
         System.out.println(max);
         return max;
-    }
 
+    }
 }
 
 
-
-
+//   public static Float getMaximum(Float a, Float b, Float c) {
+//        Float max = b;
+//       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+//            max = a;
+//        } else if (c.compareTo(max) > 0) {
+//            max = c;
+//        }
+//       System.out.println(max);
+//        return max;
+//  }
+//
+//    public static String getMaximum( String a, String b, String c) {
+//        String max = b;
+//        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+//            max = a;
+//        } else if (c.compareTo(max) > 0) {
+//            max = c;
+//        }
+//
+//        System.out.println(max);
+//        return max;
+//    }
+//
+//}
+//
+//
+//
+//
