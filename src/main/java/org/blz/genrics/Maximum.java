866adb29d168da2c7e1eb3f21bbdad313887f87b
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-11 19:23:37 +0530

    Added:float

--- src/main/java/org/blz/genrics/Maximum.java
+++ src/main/java/org/blz/genrics/Maximum.java
@@ -4,7 +4,10 @@ package org.blz.genrics;
 public class Maximum {
     public static void main(String[] args) {
         Integer a = 30, b = 10, c = 20;
+       Float d = 40f,e = 10f,f= 30f;
         getMaximum(a, b, c);
+        getMaximum(d, e, f);
+
     }
 
     public static Integer getMaximum(Integer a, Integer b, Integer c) {
@@ -14,11 +17,22 @@ public class Maximum {
         } else if (c.compareTo(max) > 0) {
             max = c;
         }
-//        System.out.println(max);
+
+        System.out.println(max);
         return max;
     }
 
 
+   public static Float getMaximum(Float a, Float b, Float c) {
+        Float max = b;
+       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            max = a;
+        } else if (c.compareTo(max) > 0) {
+            max = c;
+        }
+       System.out.println(max);
+        return max;
+  }
 }
 
 
--- src/test/java/org/blz/genrics/GenericsTest.java
+++ src/test/java/org/blz/genrics/GenericsTest.java
@@ -10,10 +10,20 @@ public class GenericsTest
 {
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue()
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1()
 
     {
       int actualResult= Maximum.getMaximum(30,10,20);
         Assert.assertEquals(30,actualResult);
     }
+
+    @Test
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue()
+    {
+        int actualResult = Maximum.getMaximum(40,10,20);
+        Assert.assertEquals(40,actualResult);
+
+    }
+
+
 }
