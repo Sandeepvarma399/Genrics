64cee0b1c187bf7065a43f3c128a3f78a507f50f
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-11 19:54:31 +0530

    Added:sting Case

--- src/main/java/org/blz/genrics/Maximum.java
+++ src/main/java/org/blz/genrics/Maximum.java
@@ -4,22 +4,24 @@ package org.blz.genrics;
 public class Maximum {
     public static void main(String[] args) {
         Integer a = 30, b = 10, c = 20;
-       Float d = 40f,e = 10f,f= 30f;
+      Float d = 40f,e = 10f,f= 30f;
+       String s1 = "apple",s2 = "peach", s3 = "banana";
         getMaximum(a, b, c);
-        getMaximum(d, e, f);
+        getMaximum(a, b, c);
+        getMaximum( a,b,c);
 
     }
 
     public static Integer getMaximum(Integer a, Integer b, Integer c) {
         Integer max = b;
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-            max = a;
-        } else if (c.compareTo(max) > 0) {
+           max = a;
+       } else if (c.compareTo(max) > 0) {
             max = c;
         }
 
-        System.out.println(max);
-        return max;
+       System.out.println(max);
+       return max;
     }
 
 
@@ -33,6 +35,19 @@ public class Maximum {
        System.out.println(max);
         return max;
   }
+
+    public static String getMaximum( String a, String b, String c) {
+        String max = b;
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            max = a;
+        } else if (c.compareTo(max) > 0) {
+            max = c;
+        }
+
+        System.out.println(max);
+        return max;
+    }
+
 }
 
 
--- src/test/java/org/blz/genrics/GenericsTest.java
+++ src/test/java/org/blz/genrics/GenericsTest.java
@@ -10,7 +10,7 @@ public class GenericsTest
 {
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1()
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue()
 
     {
       int actualResult= Maximum.getMaximum(30,10,20);
@@ -18,11 +18,18 @@ public class GenericsTest
     }
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue()
+    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue()
     {
-        int actualResult = Maximum.getMaximum(40,10,20);
-        Assert.assertEquals(40,actualResult);
+        float actualResult = Maximum.getMaximum(40f,10f,20f);
+        Assert.assertEquals(40,actualResult,0.0);
 
+    }
+
+    @Test
+    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2()
+    {
+        String actualResult = Maximum.getMaximum("apple", "peach","banana");
+        Assert.assertEquals("peach",actualResult);
     }
 
 
