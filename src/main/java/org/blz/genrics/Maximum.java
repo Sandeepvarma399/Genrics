49dd1dad687269533f53bd88498fef9b407df56d
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-14 11:16:45 +0530

    maximum number in ist position

--- src/main/java/org/blz/genrics/Maximum.java
+++ src/main/java/org/blz/genrics/Maximum.java
@@ -1,74 +1,22 @@
 package org.blz.genrics;
 
 
-public class Maximum < E extends Comparable> {
-    E value1;
-    E value2;
-    E value3;
+public class Maximum {
 
-    Maximum(E value1, E value2, E value3) {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
+    public static void main(String[] args) {
+        Integer a = 30, b = 10, c = 20;
+        getMaximum(a, b, c);
     }
-
-
-
-//    public static void main(String[] args) {
-//        Integer a = 30, b = 10, c = 20;
-//        Float d = 40f, e = 10f, f = 30f;
-//        String s1 = "apple", s2 = "peach", s3 = "banana";
-//        getMaximum(a, b, c);
-//        getMaximum(d, e, f);
-//        getMaximum(s1, s2, s3);
-//
-//    }
-
-    public E getMaximum() {
-        E result = getMaximum(value1, value2, value3);
-        return result;
-    }
-
-    public static <E extends Comparable> E getMaximum(E a, E b, E c) {
-        E max = b;
+    public static Integer getMaximum(Integer a, Integer b, Integer c) {
+        Integer max = b;
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
             max = a;
         } else if (c.compareTo(max) > 0) {
             max = c;
         }
-
         System.out.println(max);
         return max;
 
     }
+
 }
-
-
-//   public static Float getMaximum(Float a, Float b, Float c) {
-//        Float max = b;
-//       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-//            max = a;
-//        } else if (c.compareTo(max) > 0) {
-//            max = c;
-//        }
-//       System.out.println(max);
-//        return max;
-//  }
-//
-//    public static String getMaximum( String a, String b, String c) {
-//        String max = b;
-//        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-//            max = a;
-//        } else if (c.compareTo(max) > 0) {
-//            max = c;
-//        }
-//
-//        System.out.println(max);
-//        return max;
-//    }
-//
-//}
-//
-//
-//
-//
--- src/test/java/org/blz/genrics/GenericsTest.java
+++ src/test/java/org/blz/genrics/GenericsTest.java
@@ -13,24 +13,5 @@ public class GenericsTest {
         int actualResult = Maximum.getMaximum(30, 10, 20);
         Assert.assertEquals(30, actualResult);
     }
-
-    @Test
-    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue() {
-        float actualResult = Maximum.getMaximum(40f, 10f, 20f);
-        Assert.assertEquals(40, actualResult, 0.0);
-
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        String actualResult = Maximum.getMaximum("apple", "peach", "banana");
-        Assert.assertEquals("peach", actualResult);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_using_class_method_ShouldReturnCorrectValue2() {
-        Maximum maximum = new Maximum<>(1, 2, 3);
-        int actualResult = (int) Maximum.getMaximum();
-        Assert.assertEquals(3, actualResult);
-    }
 }
+
